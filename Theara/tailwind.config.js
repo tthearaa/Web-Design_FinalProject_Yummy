9ccd12e521a8b045772e367345ab45444710bf19
tthearaa <sovannthearaheang@gmail.com>
2025-12-16 10:11:59 +0700

    Merchandstuff

---
+++ Theara/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        modak: ['Modak', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+};
